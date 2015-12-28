d0ad5a6ec8ca0b20a830bf605bda306abc95d050
Ben Evans <ben@bensbit.co.uk>
2015-12-28 00:49:35 +0000

    Cover remaining branches

--- test.js
+++ test.js
@@ -2,12 +2,15 @@ const test = require('tape')
 const fadeSteps = require('./')
 
 test('fadeSteps', function (t) {
-  t.plan(4)
+  t.plan(5)
   t.deepEqual(fadeSteps('000', 'fff', 2), ['000000', 'ffffff'], 'shorthand')
   t.deepEqual(fadeSteps('000000', 'ffffff', 2), ['000000', 'ffffff'], 'longhand')
   t.deepEqual(fadeSteps('000000', 'ff00ff', 3), ['000000', '800080', 'ff00ff'], 'multistep')
   t.throws(function () {
     fadeSteps()
+  }, /Invalid Colour Hex/, 'throws on invalid colour')
+  t.throws(function () {
+    fadeSteps('22')
   }, /Invalid Colour Hex/, 'throws on invalid colour')
 })
 
